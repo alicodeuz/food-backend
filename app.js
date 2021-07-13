fddc2028c3b0c367b2c3781728214df3d8718da9
Alisher778 <alisher.musurmonov89@gmail.com>
2021-07-13 10:34:18 +0500

    :bug: Fix CORS

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+const cors = require('cors');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRoutes = require('./routes/index');
@@ -11,17 +12,13 @@ const swaggerFile = require('./swagger_output.json');
 const compression = require('compression');
 var app = express();
 
-
-app.use(compression());
+app.options('*', cors())
 app.use('*', cors());
-// require('./routes/index')(app)
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(compression());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
